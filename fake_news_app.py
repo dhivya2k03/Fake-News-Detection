92492926d14d380dd94d23f7ccc2a06e881c3f32
Dhivyabharathi U <92156958+dhivya2k03@users.noreply.github.com>
2024-02-09 15:27:36 +0530

    Add files via upload

---
+++ fake_news_app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+import lightgbm 
+
+# Load the trained model
+lgb=pickle.load(open('lgbmodekl.pkl','rb'))  # Replace 'your_model.pkl' with the path to your trained model file
+
+# Define the Streamlit UI
+st.header("Fake News Detection")
+inp = st.text_area("Enter the news you saw:", height=400, placeholder='Copy & Paste the News here..', max_chars=1000)
+
+# Make prediction when 'Submit' button is clicked
+if st.button("Submit"):
+    if inp:
+        # Vectorize the example sentence
+        vect=pickle.load(open('vector.pkl','rb'))
+
+        inp_vec = vect.transform([inp])
+        #lgbmodel= lgb.LGBMClassifier()
+        
+
+        prediction = lgb.predict(inp_vec)
+        if prediction == 1:
+            st.error(" FAKE NEWS! ❌ ")
+        else:
+            st.success("REAL NEWS! ✅")
+        
+       
+    else:
+        st.warning("Please enter a news article.")
